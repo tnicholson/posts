c16ca10485c01fe8e2917c1fc0e20262e3504675
Personal <personal@Personals-MacBook-Pro.local>
2024-06-14 17:08:38 -0700

    Quartz sync: Jun 14, 2024, 5:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 tnicholson - Dev Thoughts",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
