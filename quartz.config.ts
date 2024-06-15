df5665474b18ad5cca64a693772a3aab92ad83a4
Personal <personal@Personals-MacBook-Pro.local>
2024-06-14 17:24:50 -0700

    Quartz sync: Jun 14, 2024, 5:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "tnicholson.github.io/quartz/",
+    pageTitle: "tnicholson.github.io",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
